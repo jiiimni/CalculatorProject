85b6f4bc07d629bf997b0aaea258041b90951470
jiiimni <leejimin8951@naver.com>
2026-01-14 09:41:03 +0900

    step3: OperatorType enum

--- src/com/example/calculator/step3/OperatorType.java
+++ src/com/example/calculator/step3/OperatorType.java
@@ -1,4 +1,25 @@
 package com.example.calculator.step3;
 
-public class OperatorType {
+public enum OperatorType {
+    ADD('+'),
+    SUB('-'),
+    MUL('*'),
+    DIV('/');
+
+    private final char symbol;
+
+    OperatorType(char symbol) {
+        this.symbol = symbol;
+    }
+
+    public char getSymbol() {
+        return symbol;
+    }
+
+    public static OperatorType fromSymbol(char symbol) {
+        for (OperatorType type : values()) {
+            if (type.symbol == symbol) return type;
+        }
+        throw new IllegalArgumentException("지원하지 않는 연산자입니다.");
+    }
 }
